989e54c5ec78b77d48928bace5c5ecb138a8ce10
Nikos Oikou <nicolaosoikou@gmail.com>
2019-09-25 11:28:30 +0200

    Support multiple maps, disable db, make suitable for Heroku deployment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var mongojs = require('mongojs');
-var db = mongojs('localhost:27017/myGame', ['account', 'progress']);
+// var mongojs = require('mongojs');
+var db = null; //= mongojs('localhost:27017/myGame', ['account', 'progress']);
 
 var express = require('express');
 var app = express();
@@ -10,18 +10,29 @@ app.get('/', function(req, res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log('Server started.');
 
 var SOCKET_LIST = {};
 
-var Entity = function(){
+var Entity = function(param){
     var self = {
         x: 250,
         y: 250,
         spdX: 0,
         spdY: 0,
         id: "",
+        map: 'forest',
+    }
+    if(param){
+        if(param.x)
+            self.x = param.x;
+        if(param.y)
+            self.y = param.y;
+        if(param.map)
+            self.map = param.map;
+        if(param.id)
+            self.id = param.id;
     }
     self.update = function(){
         self.updatePosition()
@@ -36,9 +47,8 @@ var Entity = function(){
     return self;
 }
 
-var Player = function(id){
-    var self = Entity();
-    self.id = id;
+var Player = function(param){
+    var self = Entity(param);
     self.number = "" + Math.floor(10 * Math.random());
     self.pressingRight = false;
     self.pressingLeft = false;
@@ -63,9 +73,13 @@ var Player = function(id){
         }
     }
     self.shootBullet = function(angle){
-        var b = Bullet(self.id, angle);
-        b.x = self.x;
-        b.y = self.y;
+        Bullet({
+            parent: self.id,
+            angle: angle,
+            x: self.x,
+            y: self.y,
+            map: self.map,
+        });
     }
     self.updateSpd = function(){
         if(self.pressingRight)
@@ -91,6 +105,7 @@ var Player = function(id){
             hp: self.hp,
             hpMax: self.hpMax,
             score: self.score,
+            map: self.map,
         };
     }
     self.getUpdatePack = function(){
@@ -103,13 +118,19 @@ var Player = function(id){
         };
     }
 
-    Player.list[id] = self;
+    Player.list[self.id] = self;
     initPack.player.push(self.getInitPack());
     return self;
 }
 Player.list = {};
 Player.onConnect = function(socket){
-    var player = Player(socket.id);
+    var map = 'forest';
+    if(Math.random() < 0.5)
+        map = 'field';
+    var player = Player({
+        id: socket.id,
+        map: map,
+    });
     socket.on('keyPress', function(data){
         if(data.inputId === 'left')
             player.pressingLeft = data.state;
@@ -126,6 +147,7 @@ Player.onConnect = function(socket){
             player.mouseAngle = data.state;
 
         socket.emit('init', {
+            selfId: socket.id,
             player: Player.getAllInitPack(),
             bullet: Bullet.getAllInitPack(),
         })
@@ -152,12 +174,14 @@ Player.update = function(){
     return pack;
 }
 
-var Bullet = function(parent, angle){
-    var self = Entity();
+var Bullet = function(param){
+    var self = Entity(param);
     self.id = Math.random();
-    self.spdX = Math.cos(angle / 180 * Math.PI) * 10;
-    self.spdY = Math.sin(angle / 180 * Math.PI) * 10;
-    self.parent = parent;
+    self.angle = param.angle;
+    self.spdX = Math.cos(param.angle / 180 * Math.PI) * 10;
+    self.spdY = Math.sin(param.angle / 180 * Math.PI) * 10;
+    self.parent = param.parent;
+
     self.timer = 10;
     self.toRemove = false;
 
@@ -169,8 +193,9 @@ var Bullet = function(parent, angle){
 
         for(var i in Player.list){
             var p = Player.list[i];
-            if(self.getDistance(p) < 32 && self.parent !== p.id){
+            if(self.map === p.map && self.getDistance(p) < 32 && self.parent !== p.id){
                 p.hp -= 1;
+
                 if(p.hp <= 0) {
                     var shooter = Player.list[self.parent];
                     if(shooter)
@@ -188,6 +213,7 @@ var Bullet = function(parent, angle){
             id: self.id,
             x: self.x,
             y: self.y,
+            map: self.map,
         };
     }
     self.getUpdatePack = function(){
@@ -227,25 +253,28 @@ Bullet.getAllInitPack = function(){
 var DEBUG = true;
 
 var isValidPassword = function(data, cb){
-    db.account.find({username: data.username, password: data.password}, function(err, res){
-        if(res.length > 0)
-            cb(true);
-        else
-            cb(false);
-    });
+    return cb(true);
+    // db.account.find({username: data.username, password: data.password}, function(err, res){
+    //     if(res.length > 0)
+    //         cb(true);
+    //     else
+    //         cb(false);
+    // });
 }
 var isUsernameTaken = function(data, cb){
-    db.account.find({username: data.username}, function(err, res){
-        if(res.length > 0)
-            cb(true);
-        else
-            cb(false);
-    });
+    return cb(false );
+    // db.account.find({username: data.username}, function(err, res){
+    //     if(res.length > 0)
+    //         cb(true);
+    //     else
+    //         cb(false);
+    // });
 }
 var addUser = function(data, cb){
-    db.account.insert({username: data.username, password: data.password}, function(err){
-        cb();
-    });
+    return cb();
+    // db.account.insert({username: data.username, password: data.password}, function(err){
+    //     cb();
+    // });
 }
 
 var io = require('socket.io')(serv, {});
